387b8348ed5e24c195dc14f35bf296f88fcabecb
stchigel <alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2026-04-21 12:03:06 -0300

    Hecho el header/navbar de la página y el hero con animación

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Source - https://stackoverflow.com/a/54849460
+// Posted by Rathnakara S
+// Retrieved 2026-04-21, License - CC BY-SA 4.0
+
+const words = ["Stchigel", "Programador", "Diseñador", "Desarrollador", "Técnico", "Administrador"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 500);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 200);
+    };
+    loopDeleting();
+};
+
+typingEffect();
